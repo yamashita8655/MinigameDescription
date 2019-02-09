bbfc058061e6598e9c7efb8dd2feef14f41272f9
山下拓也 <>
2019-02-09 17:34:18 +0900

    シーン切り替え追加。

---
+++ Assets/MinigameScene.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameScene : MonoBehaviour {
+	[SerializeField] GameObject GameSelectRoot;
+	[SerializeField] GameObject[] GameRoots;
+	[SerializeField] GameObject BackButton;
+
+	void Start() {
+		Initialize();
+	}
+
+	private void Initialize() {
+		BackButton.SetActive(false);
+		SetGameSelectRootActive(true);
+		for (int i = 0; i < GameRoots.Length; i++) {
+			GameRoots[i].SetActive(false);
+		}
+	}
+	
+	public void OnClickGameButton(int index) {
+		for (int i = 0; i < GameRoots.Length; i++) {
+			if (i == index) {
+				GameRoots[i].SetActive(true);
+			} else {
+				GameRoots[i].SetActive(false);
+			}
+		}
+		
+		SetGameSelectRootActive(false);
+		BackButton.SetActive(true);
+	}
+
+	private void SetGameSelectRootActive(bool active) {
+		GameSelectRoot.SetActive(active);
+	}
+	
+	public void OnClickBackButton() {
+		BackButton.SetActive(false);
+		for (int i = 0; i < GameRoots.Length; i++) {
+			GameRoots[i].SetActive(false);
+		}
+		SetGameSelectRootActive(true);
+	}
+}
