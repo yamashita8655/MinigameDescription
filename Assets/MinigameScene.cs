b956bb9edc9877ca57fd0f87ccaae8ad3b94637f
山下拓也 <>
2019-02-09 18:18:31 +0900

    2個目

--- Assets/MinigameScene.cs
+++ Assets/MinigameScene.cs
@@ -15,8 +15,22 @@ public class MinigameScene : MonoBehaviour {
 	private List<int> TouchIndex = new List<int>() {
 		5,2,4,3,1,7,8,6,9,10,14,12,15,13,11,20,19,18,16,17,23,25,21,24,22
 	};
-
 	private int NowNumber = 1;
+	
+	// game2
+	[SerializeField] GameObject[] OdaiPanelObjects;
+	[SerializeField] Text MondaiText;
+	[SerializeField] Text WinText;
+	private List<string> MondaiTexts = new List<string>() {
+		"負けて", "勝って", "あいこ", "勝って", "勝って", "終わり"
+	};
+	// お題は、グー、チョキ、グー、パー、グー
+	private List<int> SeikaiIndex = new List<int>() {
+		1, 0, 0, 1, 2
+	};
+	private int MondaiCounter = 0;
+	private int WinNumber = 0;
+
 
 	void Start() {
 		Initialize();
@@ -77,6 +91,22 @@ public class MinigameScene : MonoBehaviour {
 			NowNumber++;
 		}
 	}
+	
+	public void OnClickRPSButton(int index) {
+		if (MondaiCounter >= 5) {
+			return;
+		}
+
+		int seikai = SeikaiIndex[MondaiCounter];
+		if (seikai == index) {
+			WinNumber++;
+		}
+
+		OdaiPanelObjects[MondaiCounter].SetActive(false);
+		WinText.text = WinNumber + "/5";
+		MondaiCounter++;
+		MondaiText.text = MondaiTexts[MondaiCounter];
+	}
 
 	private void InitGame1() {
 		NowNumber = 1;
@@ -86,7 +116,12 @@ public class MinigameScene : MonoBehaviour {
 		}
 	}
 	private void InitGame2() {
-	
+		for (int i = 0; i < OdaiPanelObjects.Length; i++) {
+			OdaiPanelObjects[i].SetActive(true);
+		}
+		MondaiText.text = MondaiTexts[0];
+		WinText.text = "0/5";
+		MondaiCounter = 0;
 	}
 	private void InitGame3() {
 	
